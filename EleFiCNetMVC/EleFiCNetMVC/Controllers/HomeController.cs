2d0e083ad08b016e065466d19746bad4a7b0f81f
azila200 <azila200@caledonian.ac.uk>
2023-11-07 20:43:31 +0000

    Pharmacy Page (frontend)

--- EleFiCNetMVC/EleFiCNetMVC/Controllers/HomeController.cs
+++ EleFiCNetMVC/EleFiCNetMVC/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace EleficNet.Controllers
 			return View();
 		}
 
+		public IActionResult Pharmacy()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
---
+++ EleFiCNetMVC/EleFiCNetMVC/Views/Home/Pharmacy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EleFiCNetMVC.Views.Home
+{
+    public class PharmacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
